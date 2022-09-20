04b85fa0cafdab15015267a58a21fe155bbb8e4a
gusscamargo <guga.power@hotmail.com>
2022-09-20 20:11:54 -0300

    Adicionado algumas funções e prototipos de funções

--- Grafo.py
+++ Grafo.py
@@ -1,8 +1,20 @@
 
 
-
 class Grafo:
-    pass
+    grafo = {}
+
+    def __init__(self, arquivo = "", dirigido = False):
+        self.dirigido = dirigido
+        self.enderecoArquivo = arquivo
+
+    def addAresta(self, vertice, vizinho):
+        if vertice not in self.grafo:
+            self.grafo[vertice] = [vizinho]
+        elif vizinho not in self.grafo[vertice]:
+            self.grafo[vertice].append(vizinho)
+
+    def rmAresta(self, vertice, vizinho):
+        pass
 
 if __name__ == "__main__":
     pass
