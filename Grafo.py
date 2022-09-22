543d45e6f2d12ccb645fa4fa7f2b206287542d4a
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-09-22 14:22:18 -0300

    Ajuste e agora o codigo é mais meu

--- Grafo.py
+++ Grafo.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from pyvis import network as net
 from IPython.display import display, HTML
 import plotly.graph_objects as go
-import networkx as nx
+import random
 
 class Grafo:
     grafo = defaultdict(list)
@@ -139,20 +139,21 @@ class Grafo:
         display(HTML('Grafo2D.html'))
 
 
-
-    # https://deepnote.com/@deepnote/3D-network-visualisations-using-plotly-a18c5e37-a517-4b27-bfde-1fee94a5760f
-    # https://plotly.com/python/v3/3d-network-graph/
-    # https://stackoverflow.com/questions/65752590/converting-a-networkx-2d-graph-into-a-3d-interactive-graph
-
     def showMapa3D(self):
         numVertices = self.getNumVertices()
-        G = nx.Graph()
-        G.add_edges_from(self.getArestas(tupla=True), weight = 1)
-
         edge_weights = [1] * (numVertices)
-        edges = G.edges()
+        edges = self.getArestas(tupla=True)
 
-        spring_3D = nx.spring_layout(G, dim = 3, k = 0.5)
+        spring_3D = defaultdict(list)
+
+        random.SystemRandom()
+        for i in range(1, numVertices + 1):
+            # random.seed(i)
+            spring_3D[i].extend([
+                random.random(),
+                random.random(),
+                random.random()
+            ])
 
         x_nodes = [spring_3D[key][0] for key in spring_3D.keys()]
         y_nodes = [spring_3D[key][1] for key in spring_3D.keys()]
@@ -162,36 +163,42 @@ class Grafo:
         xtp, ytp, ztp = [], [], []
 
         for edge in edges:
-            #format: [beginning,ending,None]
+            #formato: [comeco, destino, None]
             x_coords = [spring_3D[edge[0]][0], spring_3D[edge[1]][0], None]
             x_edges += x_coords
-            xtp.append(0.5*(spring_3D[edge[0]][0] + spring_3D[edge[1]][0]))
+            xtp.append(0.5 * (spring_3D[edge[0]][0] + spring_3D[edge[1]][0]))
 
             y_coords = [spring_3D[edge[0]][1], spring_3D[edge[1]][1], None]
             y_edges += y_coords
-            ytp.append(0.5*(spring_3D[edge[0]][1] + spring_3D[edge[1]][1]))
+            ytp.append(0.5 * (spring_3D[edge[0]][1] + spring_3D[edge[1]][1]))
 
             z_coords = [spring_3D[edge[0]][2], spring_3D[edge[1]][2], None]
             z_edges += z_coords
-            ztp.append(0.5*(spring_3D[edge[0]][2] + spring_3D[edge[1]][2]))
+            ztp.append(0.5 * (spring_3D[edge[0]][2] + spring_3D[edge[1]][2]))
 
 
         etext = [f'weight={w}' for w in edge_weights]
 
-        trace_weights = go.Scatter3d(x=xtp, y=ytp, z=ztp,
-                                    mode='markers',
-                                    # set the same color as for the edge lines
-                                    marker=dict(color='rgb(125,125,125)', size=1),
-                                    text=etext, hoverinfo='text')
+        trace_weights = go.Scatter3d(
+                x=xtp,
+                y=ytp,
+                z=ztp,
+                mode='markers',
+                # set the same color as for the edge lines
+                marker=dict(color='rgb(125,125,125)', size=1),
+                text=etext, 
+                hoverinfo='text'
+            )
 
         #create a trace for the edges
         trace_edges = go.Scatter3d(
-            x=x_edges,
-            y=y_edges,
-            z=z_edges,
-            mode='lines',
-            line=dict(color='black', width=2),
-            hoverinfo='none')
+                x=x_edges,
+                y=y_edges,
+                z=z_edges,
+                mode='lines',
+                line=dict(color='black', width=2),
+                hoverinfo='none'
+            )
 
         #create a trace for the nodes
         trace_nodes = go.Scatter3d(
@@ -209,103 +216,6 @@ class Grafo:
         fig = go.Figure(data=data)
 
         fig.show()
-
-
-
-    # def showMapa3D(self):
-        # numVertices, Arestas = self.getNumVertices(), self.getArestas()
-
-        # # Gerar posições aleatorias para os nodos num plano 3D
-        # # locs = []
-        # # for i in range(numVertices + 1):
-        # #     random.seed(i)
-        # #     locs.append([
-        # #         random.random(),
-        # #         random.random(),
-        # #         random.random()
-        # #     ])
-
-        # locs = nx.spring_layout(Arestas, dim = 3, k = 0.5)
-
-        # # Configs
-        # x_vertices = [locs[i][0] for i in range(numVertices)]
-        # y_vertices = [locs[i][1] for i in range(numVertices)]
-        # z_vertices = [locs[i][2] for i in range(numVertices)]
-
-        # x_arestas, y_arestas, z_arestas = [], [], []
-
-        # for aresta in Arestas:
-        #     x_arestas.append([
-        #         locs[aresta[0]][0], locs[aresta[1]][0], None
-        #     ])
-            
-        #     y_arestas.append([
-        #         locs[aresta[0]][1], locs[aresta[1]][1], None
-        #     ])
-
-        #     z_arestas.append([
-        #         locs[aresta[0]][2], locs[aresta[1]][2], None
-        #     ])
-
-        # trace_arestas = go.Scatter3d(
-        #     x = x_arestas,
-        #     y = y_arestas,
-        #     z = z_arestas,
-        #     mode = "lines",
-        #     line = dict(
-        #         color = "rgb(125,125,125)",
-        #         width = 1
-        #     ),
-        #     hoverinfo="none"
-        # )
-
-        # trace_vertices = go.Scatter3d(
-        #     x = x_vertices,
-        #     y = y_vertices,
-        #     z = z_vertices,
-        #     mode = "markers",
-        #     marker = dict(
-        #         symbol = "circle",
-        #         size = 10,
-        #         color = "blue",
-        #         line = dict(
-        #             color = "black",
-        #             width = 0.5
-        #         )
-        #     ),
-        #     text = [*map(str, self.getVertices())],
-        #     hoverinfo = "text"
-        # )
-
-        # axis = dict(
-        #     showbackground=False,
-        #     showline=True,
-        #     zeroline=False,
-        #     showgrid=False,
-        #     showticklabels=False,
-        #     title=''
-        # )
-
-        # layout = go.Layout(
-        #     title = "Grafo 3D",
-        #     autosize=True,
-        #     showlegend=False,
-        #     scene=dict(xaxis=dict(axis),
-        #             yaxis=dict(axis),
-        #             zaxis=dict(axis),
-        #             ),
-        #     margin=dict(t=100),
-        #     hovermode='closest'
-        # )
-
-        # data = [trace_vertices, trace_arestas]
-
-        # # Gerar figura 3D
-        # fig = go.Figure(
-        #     data=data, 
-        #     layout=layout
-        #     )
-        # fig.show()
 
 
 
