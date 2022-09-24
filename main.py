b9bc438be09d7763e8c5f8614bac0feadaa3ab2e
gusscamargo <guga.power@hotmail.com>
2022-09-23 21:52:35 -0300

    ajustes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Grafo import GrafoFromFile
 
 
 if __name__ == "__main__":
-    g = GrafoFromFile("grafo_virgula.txt", separador=",")
+    g = GrafoFromFile("grafo.txt", separador=",")
     
-    # g.showMapa2D()
+    g.showMapa2D()
     g.showMapa3D()
