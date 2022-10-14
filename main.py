0928eb77962179d89322d3846639c552c6c04498
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-14 15:54:33 -0300

    Ajustes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Grafo import GrafoFromFile
 
 
 if __name__ == "__main__":
-    g = GrafoFromFile("teste.txt", separador=",")
+    g = GrafoFromFile("grafo.txt", separador=",")
     
     # Imprime os Grafos em 2D e 3D
     g.showMapa2D()
