37bc583c48e9efacbcf3b88469505638b056ff94
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-04-02 23:24:52 -0300

    Criando sincronia

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from Grafo import GrafoFromFile
+from Grafo import graph_from_file
 
 
 if __name__ == "__main__":
-    g = GrafoFromFile("grafo.txt", separador=",")
-    
+    g = graph_from_file("grafo.txt", separator=",")
+    # print(g.get_edges())
     # Imprime os Grafos em 2D e 3D
-    g.showMapa2D()
-    # g.showMapa3D()
+    # g.show_2d_map()
+    g.show_3d_map()
