62ca99e5c0109a0765802e1541d97a3fc73a4802
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-04-02 23:22:56 -0300

    Revisão e refatoração do codigo

--- Grafo.py
+++ Grafo.py
@@ -4,208 +4,179 @@ from IPython.display import display, HTML
 import plotly.graph_objects as go
 import random
 import minify_html
+from typing import List, Tuple, Union
 
-class Grafo:
-    grafo = defaultdict(list)
 
-    def __init__(self, dirigido = False) -> None:
-        self.dirigido = dirigido
+class Graph:
+    def __init__(self, directed: bool = False) -> None:
+        self.graph = defaultdict(list)
+        self.directed = directed
 
-    def addAresta(self, vertice, vizinho) -> object:
-        if vizinho not in self.grafo[vertice]:
-            self.grafo[vertice].append(vizinho)
-            if not self.dirigido:
-                self.grafo[vizinho].append(vertice)
+    def add_edge(self, vertex: any, neighbor: any) -> None:
+        if neighbor not in self.graph[vertex]:
+            self.graph[vertex].append(neighbor)
+            if not self.directed:
+                self.graph[neighbor].append(vertex)
 
-        return self
+    def remove_edge(self, vertex: any, neighbor: any) -> None:
+        if vertex in self.graph:
+            self.graph[vertex].remove(neighbor)
+            if not self.directed:
+                self.graph[neighbor].remove(vertex)
 
-    def rmAresta(self, vertice, vizinho) -> object:
-        if vertice in self.grafo:
-            self.grafo[vertice].remove(vizinho)
-            if not self.dirigido:
-                self.grafo[vizinho].remove(vertice)
-        
-        return self
+    def get_num_vertices(self) -> int:
+        return len(self.graph.keys())
 
-    def getNumVertices(self) -> int:
-        return len(self.grafo.keys())
+    def get_degree(self, vertex: any) -> int:
+        return len(self.graph[vertex])
 
-    def getGrau(self, s) -> int:
-        return len(self.grafo[s])
+    def get_vertices(self) -> List[any]:
+        return list(self.graph.keys())
 
-    def getVertices(self) -> list:
-        return self.grafo.keys()
+    def bfs(self, vertex: any) -> List[any]:
+        visited, queue, result = [vertex], [vertex], []
 
-    def bfs(self, s) -> list:
-        visitados, fila, resultado = [s], [s], []
+        while queue:
+            vertex = queue.pop(0)
+            for neighbor in self.graph[vertex]:
+                if neighbor not in visited:
+                    visited.append(neighbor)
+                    queue.append(neighbor)
+            result.append(vertex)
 
-        while fila:
-            s = fila.pop(0)
-            for vertice in self.grafo[s]:
-                if vertice not in visitados:
-                    visitados.append(vertice)
-                    fila.append(vertice)
-            resultado.append(s)
+        return result
 
-        return resultado
+    def dfs(self, vertex: any) -> List[any]:
+        stack, path = [vertex], []
+        while stack:
+            vertex = stack.pop()
+            if vertex not in path:
+                path.append(vertex)
+                stack.extend(self.graph[vertex])
 
-    def dfs(self, s) -> list:
-        pilha, caminho = [s], []
-        while pilha:
-            vertice = pilha.pop()
-            if vertice not in caminho:
-                caminho.append(vertice)
-                pilha.extend(self.grafo[vertice])
-        
-        return caminho
+        return path
 
-    def __temCiclo(self, s, visitado, parente) -> bool:
-        visitado[s] = True
+    def __has_cycle(self, vertex: any, visited: List[bool], parent: any) -> bool:
+        visited[vertex] = True
 
-        for i in self.grafo[s]:
-            if not visitado[i]:
-                if self.__temCiclo(i, visitado, s):
+        for neighbor in self.graph[vertex]:
+            if not visited[neighbor]:
+                if self.__has_cycle(neighbor, visited, vertex):
                     return True
-            elif parente != i:
+            elif parent != neighbor:
                 return True
 
         return False
 
-    def temCiclo(self) -> bool:
-        numVertices: int = self.getNumVertices()
-        visitado: list = [False] * (numVertices + 1)
+    def has_cycle(self) -> bool:
+        num_vertices = self.get_num_vertices()
+        visited = [False] * (num_vertices + 1)
 
-        for i in range(numVertices):
-            if not visitado[i]:
-                if self.__temCiclo(i, visitado, -1):
+        for vertex in range(num_vertices):
+            if not visited[vertex]:
+                if self.__has_cycle(vertex, visited, -1):
                     return True
 
         return False
+    
+    def get_edges(self, as_tuple: bool = False) -> List[Union[Tuple[any, any], List[any]]]:
+        result = []
+        processed_edges = set()
 
-    def showVertices(self) -> object:
-        vertices = self.getVertices()
-        
-        print("Vertices:")
-        for i, s in enumerate(vertices):
-            print(f'{s}', end = "")
-            if i % 10 == 0:
-                print()
+        for vertex in self.get_vertices():
+            for neighbor in self.graph[vertex]:
+                edge = (vertex, neighbor) if vertex < neighbor else (neighbor, vertex)
+                if not self.directed and edge in processed_edges:
+                    continue
+                processed_edges.add(edge)
 
-        return self
-
-    def showArestas(self) -> object:
-        vertices = self.getVertices()
-        i = 0
-        for s1 in vertices:
-            for s2 in self.grafo[s1]:
-                print(f'({s1}, {s2})', end = ", ")
-                i += 1
-                if i % 10 == 0:
-                    print()
-
-        return self
-
-    def getArestas(self, tupla = False) -> list:
-        resultado, vertices = [], self.getVertices()
-
-        for s1 in vertices:
-            for s2 in self.grafo[s1]:
-                if tupla:
-                    resultado.append((s1, s2))
+                if as_tuple:
+                    result.append(edge)
                 else:
-                    resultado.append([s1, s2])
+                    result.append([vertex, neighbor])
 
-        return resultado
+        return result
 
-    def showMapa2D(self) -> None:
-        Vertices, Arestas = self.getVertices(), self.getArestas()
+    def __str__(self) -> str:
+        vertices_str = ", ".join(map(str, self.get_vertices()))
+        edges_str = ", ".join([f"({v1}, {v2})" for v1, v2 in self.get_edges(as_tuple=True)])
 
-        # congig
+        return f"Vertices: {vertices_str}\nEdges: {edges_str}"
+
+
+    def show_2d_map(self) -> None:
+        vertices, edges = self.get_vertices(), self.get_edges()
+        print(vertices)
+        print(edges)
+
         interface = net.Network(
-                height='100%', 
-                width='100%',
-                notebook=True, 
-                heading='Grafo 2D'
-            )
-        
-        # Add Vertices
-        for v in Vertices:
+            height='100%',
+            width='100%',
+            notebook=True,
+            heading='2D Graph'
+        )
+
+        for v in vertices:
             interface.add_node(v, label=str(v))
 
-        # Add Arestas
-        interface.add_edges(Arestas)
+        interface.add_edges(edges)
 
-        # export interfaces
-        nome_arquivo_saida = 'Grafo2D.html'
-        interface.show(nome_arquivo_saida)
-        self.__minificarArquivoHTML(nome_arquivo_saida)
-        display(HTML(nome_arquivo_saida))
-
-    def __minificarArquivoHTML(self, nomeArquivo="Grafo2D.html"):
-        arquivoRead, arquivoWrite = open(nomeArquivo, "r").read(), open(nomeArquivo, "w+")
-        arquivoWrite.write(minify_html.minify(arquivoRead, minify_js=True, remove_processing_instructions=True, minify_css=True))
-        arquivoWrite.close()
+        output_file = '2DGraph.html'
+        interface.show(output_file)
+        display(HTML(output_file))
 
 
-    def showMapa3D(self) -> None:
-        numVertices = self.getNumVertices()
-        arestas_weights = [1] * (numVertices)
-        arestas = self.getArestas(tupla=True)
 
-        grafo_3D = defaultdict(list)
+    def show_3d_map(self) -> None:
+        num_vertices = self.get_num_vertices()
+        edges_weights = [1] * num_vertices
+        edges = self.get_edges(as_tuple=True)
 
-        # random.SystemRandom torna o aleatorio em aleatorio imprevisivel
-        random.SystemRandom()
-        grafo_3D = {
-            v: [random.random(), random.random(), random.random()] for v in self.getVertices()
+        graph_3d = {
+            v: [random.random(), random.random(), random.random()] for v in self.get_vertices()
         }
 
-        x_vertices = [*map(lambda key: grafo_3D[key][0], grafo_3D.keys())]
-        y_vertices = [*map(lambda key: grafo_3D[key][1], grafo_3D.keys())]
-        z_vertices = [*map(lambda key: grafo_3D[key][2], grafo_3D.keys())]
+        x_vertices = [graph_3d[key][0] for key in graph_3d.keys()]
+        y_vertices = [graph_3d[key][1] for key in graph_3d.keys()]
+        z_vertices = [graph_3d[key][2] for key in graph_3d.keys()]
 
-        x_arestas, y_arestas, z_arestas = [], [], []
+        x_edges, y_edges, z_edges = [], [], []
         xtp, ytp, ztp = [], [], []
 
-        for aresta in arestas:
-            #formato: [comeco, destino, None]
-            x_coords = [grafo_3D[aresta[0]][0], grafo_3D[aresta[1]][0], None]
-            x_arestas.extend(x_coords)
-            xtp.append(0.5 * (grafo_3D[aresta[0]][0] + grafo_3D[aresta[1]][0]))
+        for edge in edges:
+            x_coords = [graph_3d[edge[0]][0], graph_3d[edge[1]][0], None]
+            x_edges.extend(x_coords)
+            xtp.append(0.5 * (graph_3d[edge[0]][0] + graph_3d[edge[1]][0]))
 
-            y_coords = [grafo_3D[aresta[0]][1], grafo_3D[aresta[1]][1], None]
-            y_arestas.extend(y_coords)
-            ytp.append(0.5 * (grafo_3D[aresta[0]][1] + grafo_3D[aresta[1]][1]))
+            y_coords = [graph_3d[edge[0]][1], graph_3d[edge[1]][1], None]
+            y_edges.extend(y_coords)
+            ytp.append(0.5 * (graph_3d[edge[0]][1] + graph_3d[edge[1]][1]))
 
-            z_coords = [grafo_3D[aresta[0]][2], grafo_3D[aresta[1]][2], None]
-            z_arestas.extend(z_coords)
-            ztp.append(0.5 * (grafo_3D[aresta[0]][2] + grafo_3D[aresta[1]][2]))
+            z_coords = [graph_3d[edge[0]][2], graph_3d[edge[1]][2], None]
+            z_edges.extend(z_coords)
+            ztp.append(0.5 * (graph_3d[edge[0]][2] + graph_3d[edge[1]][2]))
 
-
-        etext = [f'weight={w}' for w in arestas_weights]
+        etext = [f'weight={w}' for w in edges_weights]
 
         trace_weights = go.Scatter3d(
-                x=xtp,
-                y=ytp,
-                z=ztp,
-                mode='markers',
-                # Cor das linhas
-                marker=dict(color='rgb(125,125,125)', size=1),
-                text=etext, 
-                hoverinfo='text'
-            )
+            x=xtp,
+            y=ytp,
+            z=ztp,
+            mode='markers',
+            marker=dict(color='rgb(125,125,125)', size=1),
+            text=etext,
+            hoverinfo='text'
+        )
 
-        # Criar Linhas
-        trace_arestas = go.Scatter3d(
-                x=x_arestas,
-                y=y_arestas,
-                z=z_arestas,
-                mode='lines',
-                line=dict(color='black', width=2),
-                hoverinfo='none'
-            )
+        trace_edges = go.Scatter3d(
+            x=x_edges,
+            y=y_edges,
+            z=z_edges,
+            mode='lines',
+            line=dict(color='black', width=2),
+            hoverinfo='none'
+        )
 
-        # Criar Vertices
         trace_vertices = go.Scatter3d(
             x=x_vertices,
             y=y_vertices,
@@ -217,15 +188,14 @@ class Grafo:
         )
 
         layout = go.Layout(
-                title = "Grafo 3D",
-                autosize=True,
-                showlegend=True,
-                margin=dict(t=100),
-                hovermode='closest'
-            )
+            title="3D Graph",
+            autosize=True,
+            showlegend=True,
+            margin=dict(t=100),
+            hovermode='closest'
+        )
 
-        # Juntar todas as infos
-        data = [trace_arestas, trace_vertices, trace_weights]
+        data = [trace_edges, trace_vertices, trace_weights]
         fig = go.Figure(data=data, layout=layout)
 
         fig.show()
@@ -233,15 +203,16 @@ class Grafo:
 
 
 
-def GrafoFromFile(enderecoArquivo = "", separador = " ", dirigido = False) -> Grafo:
-    grafo = Grafo(dirigido)
-    split = lambda string: [*map(int, string.split(separador))]
 
-    with open(enderecoArquivo, "r") as arquivo:
-        for linha in arquivo.readlines():
-            aresta = split(linha)
-            if len(aresta) > 1:
-                a, b = aresta
-                grafo.addAresta(a, b)
+def graph_from_file(file_path: str = "", separator: str = " ", directed: bool = False) -> Graph:
+    graph = Graph(directed)
+    split = lambda string: [*map(int, string.split(separator))]
 
-    return grafo
+    with open(file_path, "r") as file:
+        for line in file.readlines():
+            edge = split(line)
+            if len(edge) > 1:
+                a, b = edge
+                graph.add_edge(a, b)
+
+    return graph
